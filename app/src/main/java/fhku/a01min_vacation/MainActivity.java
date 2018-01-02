e6128f8b4be13a2b279f07cb5c7dbc5530ec1aca
daempflok <chr.goerner@gmail.com>
2018-01-02 12:07:57 +0100

    added intents

--- app/src/main/java/fhku/a01min_vacation/MainActivity.java
+++ app/src/main/java/fhku/a01min_vacation/MainActivity.java
@@ -1,23 +1,43 @@
 package fhku.a01min_vacation;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Button createButton;
+    public Button showButton;
+
+    public void init(){
+        createButton=(Button)findViewById(R.id.create);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,CreateActivity.class);
+                startActivity(intent);
+            }
+        });
+        showButton=(Button)findViewById(R.id.play);
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,ShowActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
     }
 
-
-    public void create(View view) {
-        setContentView(R.layout.activity_create);
-    }
-
-    public void show(View view) {
-        setContentView(R.layout.activity_show);
-    }
 }
