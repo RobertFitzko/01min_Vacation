14d8ebc1d78c4b3ae51ebaefb8c235e5f989eab3
Melanie Dehne <medehn@gmail.com>
2018-01-11 10:17:21 +0100

    View für BilderAuswahl

---
+++ app/src/main/java/fhku/a01min_vacation/SelectPicsActivity.java
@@ -0,0 +1,13 @@
+package fhku.a01min_vacation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SelectPicsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_pics);
+    }
+}
