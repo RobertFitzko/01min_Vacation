e3871c8cceb3659764d33f6c7ea421e41bd908d5
RobertFitzko <robertfitzko@gmx.net>
2018-02-01 11:25:47 +0100

    speichern Bilder

--- app/src/main/java/fhku/a01min_vacation/SelectPicsActivity.java
+++ app/src/main/java/fhku/a01min_vacation/SelectPicsActivity.java
@@ -65,7 +65,7 @@ public class SelectPicsActivity extends AppCompatActivity {
                 public void onClick(View v) {
 
                     //impliziter Intent für Gallerie
-                    Intent imagePickerIntent = new Intent(Intent.ACTION_PICK);
+                    Intent imagePickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
                     //Wo sind die Bilder?
                     File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
                     String pictureDirectoryPath = pictureDirectory.getPath();
@@ -76,8 +76,6 @@ public class SelectPicsActivity extends AppCompatActivity {
                     //Data und Typ setzen, in diesem Fall alle Bild-Dateitypen
                     imagePickerIntent.setDataAndType(data, "image/*");
 
-                    //Activity for Result aufrufen
-                    startActivityForResult(imagePickerIntent, IMAGE_GALLERY_REQUEST);
 
                     //switch case um jeweils Bild auf den jeweils geklickten Button zuzuweisen
                     switch (v.getId()) {
@@ -102,9 +100,15 @@ public class SelectPicsActivity extends AppCompatActivity {
                         default:
                             throw new RuntimeException("Unknow button ID");
                     }
+
+                    //Activity for Result aufrufen
+                    startActivityForResult(imagePickerIntent, IMAGE_GALLERY_REQUEST);
+
                 }
             });
         }
+
+        loadImages();
     }
 
     @Override
@@ -114,26 +118,61 @@ public class SelectPicsActivity extends AppCompatActivity {
             //wenn es sich wirklich um einen aufruf von unseren buttons handelt
             if(requestCode==IMAGE_GALLERY_REQUEST){
                 Uri imageUri = data.getData();
-                picUri[currentImageButton] = imageUri;
-                Log.i("PicUri = ",""+ picUri[currentImageButton]);
-                //input stream für Bilder mit nötigem trycatch
-                InputStream inputStream;
 
-                try {
-                    inputStream = getContentResolver().openInputStream(imageUri);
-                    //get bitmap from stream
-                    Bitmap image = BitmapFactory.decodeStream(inputStream);
+                Log.i("PicUri = ",""+ imageUri);
 
-                    //image wird auf den jeweiligen button gesetzt falls frei
-                        images[currentImageButton].setImageBitmap(image);
-
-                    //falls das Bild nicht gefunden werden kann
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this,"Bild konnte nicht gefunden werden",Toast.LENGTH_LONG).show();
-                }
+                this.saveImage(currentImageButton, imageUri);
+                this.loadImage(imageUri);
             };
         };
+
+    }
+
+    public void loadImages() {
+
+        for (int i = 0; i < 6; i++) {
+            String uri = saveData.getString("image" + i, null);
+
+            Log.i("LOAD IMAGES", uri + "" + " index: " + i);
+
+            if (uri != null) {
+                Uri parsedUri = Uri.parse(uri);
+                picUri[i] = parsedUri;
+                loadImage(parsedUri);
+            }
+
+        }
+
+    }
+
+    public void saveImage(int index, Uri imageUri) {
+        picUri[index] = imageUri;
+
+        SharedPreferences.Editor editor = saveData.edit();
+        editor.putString("image" + index, imageUri.toString());
+        editor.commit();
+
+        Log.i("SAVE IMAGES", "Uri: " + imageUri + ", index: " + index);
+    }
+
+    public void loadImage(Uri imageUri) {
+
+        //input stream für Bilder mit nötigem trycatch
+        InputStream inputStream;
+
+        try {
+            inputStream = getContentResolver().openInputStream(imageUri);
+            //get bitmap from stream
+            Bitmap image = BitmapFactory.decodeStream(inputStream);
+
+            //image wird auf den jeweiligen button gesetzt falls frei
+            images[currentImageButton].setImageBitmap(image);
+
+            //falls das Bild nicht gefunden werden kann
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this,"Bild konnte nicht gefunden werden",Toast.LENGTH_LONG).show();
+        }
 
     }
 
